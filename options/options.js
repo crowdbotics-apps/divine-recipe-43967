2505e758d8ade266e067db8b5d38e7a96dc3f332
Crowdbotics <team@crowdbotics.com>
2023-10-16 14:59:28 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "divine-recipe-43967",
+  url: "https://divine-recipe-43967.botics.co",
+  api: "https://divine-recipe-43967.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
